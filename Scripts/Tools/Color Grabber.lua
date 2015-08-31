fdadd9df7ddf5ac0b1b55336606c3beaf6777110
IMACH <mr.kevinmach@gmail.com>
2015-08-31 18:03:51 +0800

    Added Daily 3/3
    Orientation and RBG Color Codes

---
+++ Scripts/Tools/Color Grabber.lua
@@ -0,0 +1,9 @@
+-- Test Script 
+-- used to get colors at coordinates 
+
+--Daily Counter 3/3 
+local rgb1 = getColor(121,171); 
+log("daily" .. "rgb1_" .. rgb1); 
+
+local rgb2 = getColor(108,163); 
+log("daily" .. "rgb2_" .. rgb2);
