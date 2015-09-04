92df7d2b12832b5f2ac91ff51e2eb8c4948e9e6e
IMACH <mr.kevinmach@gmail.com>
2015-09-04 18:28:50 +0800

    Cleanup, ScreenResolutionTool

---
+++ ScreenResolutionTool.lua
@@ -0,0 +1,31 @@
+local width, height = getScreenResolution(); 
+log("Width" .. width); 
+log("Height" .. height); 
+
+regionCalculate = function (topLeftHP,topLeftWP,heightP, widthP) 
+-- P represents Percentage, 
+-- heightP and widthP are found by find the distance of upper percentage to lower percentage 
+local coordinateH = 0; 
+local coordinateW = 0; 
+local regionHeight = 0; 
+local regionWidth = 0; 
+
+coordinateH = (height * topLeftHP); 
+coordinateW = (width * topLeftWP); 
+log("CoordinateH" .. coordinateH); 
+log("CoordinateW" .. coordinateW); 
+
+regionHeight = (height * heightP);
+log("Height" .. regionHeight);  
+regionWidth = (width * widthP); 
+log("Width" .. regionWidth); 
+
+local region = {coordinateH,coordinateW,regionHeight,regionWidth}; 
+-- screenshot not working atm 
+screenshot("FutureFight/mission.png",nil);
+
+end 
+
+
+
+regionCalculate(.60,.85,.13,.25);
